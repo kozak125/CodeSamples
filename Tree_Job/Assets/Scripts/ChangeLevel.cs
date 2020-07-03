e597cc046e391369a115377294244c2010240c40
Janusz Kozakiewicz <xiaolin125@gazeta.pl>
2020-07-03 17:18:19 +0200

    Playable demo

---
+++ Tree_Job/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class used to change levels
+/// </summary>
+public class ChangeLevel : MonoBehaviour
+{
+    /// <summary>
+    /// Reload current scene
+    /// </summary>
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Tree_Job/Assets/Scripts/Statics/EventBroker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Class used to broker events between clases. Part of Publisher/Subscriber pattern
+/// </summary>
+public class EventBroker
+{
+    /// <summary>
+    /// Event used to signal, that animation has ended
+    /// </summary>
+    public static event Action OnAnimationEnded;
+
+    /// <summary>
+    /// Raises the OnAnimationEnded event, while there are listeners to it
+    /// </summary>
+    public static void CallOnAnimationEnded()
+    {
+        OnAnimationEnded?.Invoke();
+    }
+
+    /// <summary>
+    /// Event used to signal, that tree has been destroyed
+    /// </summary>
+    public static event Action OnTreeDestroyed;
+
+    /// <summary>
+    /// Raises the OnTreeDestroyed event, while there are listeners to it
+    /// </summary>
+    public static void CallOnTreeDestroyed()
+    {
+        OnTreeDestroyed?.Invoke();
+    }
+
+    /// <summary>
+    /// Event used to signal, that tree is about to change
+    /// </summary>
+    public static event Action<Vector3> OnTreeChanging;
+
+    /// <summary>
+    /// Raises the OnTreeChanging event, while there are listeners to it
+    /// </summary>
+    public static void CallOnTreeChanging(Vector3 distanceBetweenTrees)
+    {
+        OnTreeChanging?.Invoke(distanceBetweenTrees);
+    }
+
+    /// <summary>
+    /// Event used to signal, that game has ended
+    /// </summary>
+    public static event Action OnGameEnded;
+
+    /// <summary>
+    /// Raises the OnGameEnded event, while there are listeners to it
+    /// </summary>
+    public static void CallOnGameEnded()
+    {
+        OnGameEnded?.Invoke();
+    }
+}
